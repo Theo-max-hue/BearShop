0845e08de6af86ceae7774994b94458a530bd95e
Theo-max-hue <theo.gamard@orange.fr>
2022-05-31 18:57:19 +0200

    premier commit

---
+++ cmake-build-debug/CMakeFiles/Projet_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_c.dir/main.c.o"
+  "Projet_c"
+  "Projet_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
